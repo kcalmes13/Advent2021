d2dde5111e6b17bdc50eb5315f353f308fcae4fd
Kaleb Calmes <kcalmes@unionwireless.com>
2021-12-03 12:23:57 -0700

    advent day 3 done

--- Advent2021/BinaryDiag/Program.cs
+++ Advent2021/BinaryDiag/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinaryDiag;
+
+List<string> inputs = System.IO.File.ReadLines(@"c:\advent\day3\input.txt").ToList();
+string gamma = string.Empty;
+
+for (int i =0; i < inputs.First().Length; i++)
+{
+    List<int> values = inputs.Select(x => x[i].Equals('0') ? -1 : 1).ToList();
+
+    gamma += values.Sum() > 0 ? "1" : "0";
+}
+
+var gammaVal = Convert.ToInt64(gamma, 2);
+string eps = new string(gamma.Select(x => x.Equals('0') ? '1' : '0').ToArray());
+var epsVal = Convert.ToInt64(eps, 2);
+var total = gammaVal * epsVal;
+
+Console.WriteLine(total);
+
+string oxy = recurseEz.RecurseVals(inputs, true, 0);
+string carb = recurseEz.RecurseVals(inputs, false, 0);
+
+total = Convert.ToInt64(oxy, 2) * Convert.ToInt64(carb, 2);
+
+Console.WriteLine(total);
---
+++ Advent2021/BinaryDiag/recurseEz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryDiag
+{
+    internal static class recurseEz
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <param name="most"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        internal static string RecurseVals(List<string> inputs, bool most, int index)
+        {
+            List<int> values = inputs.Select(x => x[index].Equals('0') ? -1 : 1).ToList();
+
+            List<string> remaining = new List<string>();
+            if (values.Sum() == 0)
+                remaining = inputs.Where(x => (x[index] == '1' && most) || (x[index] == '0' && !most)).ToList();
+            else
+            {
+                char bitValue = (values.Sum() > 0 && most) || (values.Sum() < 0 && !most) ? '1' : '0';
+                remaining = inputs.Where(x => x[index].Equals(bitValue)).ToList();
+            }           
+            
+            string foundVal = remaining.Count > 1 ?
+                RecurseVals(remaining, most, index + 1) :
+                remaining.First();
+
+            return foundVal;
+        }
+    }
+}
