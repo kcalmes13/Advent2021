d7db806309ae40fa9493259903166e728dd0ff41
Kaleb Calmes <kcalmes@unionwireless.com>
2021-12-04 13:23:02 -0700

    day 4 done

---
+++ Advent2021/Bingo/CheckBingo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    internal static class CheckBingo
+    {
+        internal static List<int[][]> BuildBoards(List<string> inputs, int rowsPerBoard)
+        {
+            // build boards
+            List<int[][]> boards = new List<int[][]>();
+
+            while (inputs.Count > 0)
+            {
+                List<string> boardVals = inputs.Take(rowsPerBoard).ToList();
+                int[][] board = new int[rowsPerBoard][];
+
+                for (int i = 0; i < boardVals.Count; i++)
+                {
+                    var vals = boardVals[i].Split().ToList();
+                    vals.RemoveAll(x => x.Equals(string.Empty));
+                    board[i] = vals.Select(x => int.Parse(x)).ToArray();
+                }
+
+                boards.Add(board);
+                inputs.RemoveRange(0, 5);
+            }
+            
+            return boards;
+        }
+
+        internal static int TimeToBingo(int[][] board, List<int> draws)
+        {
+            List<List<int>> boardCombos = new List<List<int>>();
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                boardCombos.Add(board[i].ToList());
+                List<int> row = new List<int>();
+
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    row.Add(board[j][i]);
+                }
+
+                boardCombos.Add(row);
+            }
+
+            List<int> drawn = draws.Take(board.Length).ToList();
+            int count = board.Length;
+            while (!boardCombos.Any(x => x.All(z => drawn.Contains(z))) || count > draws.Count)
+            {
+                count++;
+                drawn.Add(draws[count - 1]);
+            }
+
+            return count;
+        }
+
+        internal static int CalculateScore(int[][] board, List<int> draws)
+        {
+            int total = 0;
+
+            foreach (var row in board)
+            {
+                total += row.Where(x => !draws.Contains(x)).Sum();
+            }
+
+            total = total * draws.Last();
+
+            return total;
+        }
+    }    
+}
---
+++ Advent2021/Bingo/Program.cs
@@ -0,0 +1,53 @@
+﻿using Bingo;
+
+List<string> inputs = System.IO.File.ReadLines(@"c:\advent\day4\input.txt").ToList();
+
+// build your draws
+List<int> draws = inputs.First().Split(',').Select(x => int.Parse(x)).ToList();
+inputs.RemoveRange(0, 2);
+
+// get index of first empty (can use directly thank to 1 row being index 0)
+int rowCount =  inputs.IndexOf(string.Empty);
+inputs.RemoveAll(x => x.Equals(string.Empty));
+
+// build boards
+List<int[][]> boards = CheckBingo.BuildBoards(inputs, rowCount);
+
+int count = -1;
+int[][] winningBoard = new int[5][];
+
+foreach(int[][] board in boards)
+{
+    var lowestBingo = CheckBingo.TimeToBingo(board, draws);
+
+
+    if (count == -1 || lowestBingo < count)
+    {
+        count = lowestBingo;
+        winningBoard = board;
+    }
+}
+
+List<int> neededDraws = draws.Take(count).ToList();
+int total = CheckBingo.CalculateScore(winningBoard, neededDraws);
+
+Console.WriteLine(total);
+
+count = -1;
+int[][] losingBoard = new int[5][];
+
+foreach (int[][] board in boards)
+{
+    var highestBingo = CheckBingo.TimeToBingo(board, draws);
+
+    if (count == -1 || highestBingo > count)
+    {
+        count = highestBingo;
+        losingBoard = board;
+    }
+}
+
+neededDraws = draws.Take(count).ToList();
+total = CheckBingo.CalculateScore(losingBoard, neededDraws);
+
+Console.WriteLine(total);
