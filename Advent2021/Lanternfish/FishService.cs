c5d9c8c33d47d0d50222887eab86414338f66590
Kaleb Calmes <kcalmes@unionwireless.com>
2021-12-07 14:45:13 -0700

    advent day 6

---
+++ Advent2021/Lanternfish/FishService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanternfish
+{
+    internal static class FishService
+    {
+        internal static int CaclulateFish(int days, List<int> startingFish)
+        {
+            List<int> currentFish = startingFish;
+
+            while (days > 0)
+            {
+                int startCount = currentFish.Count;
+                for (int i = 0; i < startCount; i++)
+                {
+                    switch(currentFish[i])
+                    {
+                        case 0:
+                            currentFish[i] = 6;
+                            currentFish.Add(8);
+                            break;
+                        default:
+                            currentFish[i]--;
+                            break;
+                    }
+                }
+
+                days--;
+
+                if (days == 100)
+                {
+                    var c = 0;
+                }
+            }
+
+            return currentFish.Count();
+        }
+
+        internal static double CaclulateFishV2(int days, List<int> startingFish)
+        {
+            List<int> currentFish = startingFish;
+            double[] fishAmounts = new double[9];
+
+            foreach (int fish in currentFish)
+            {
+                fishAmounts[fish]++;
+            }
+
+            while (days > 0)
+            {
+                double[] newFish = new double[9];
+                for (int i = 8; i >= 0; i--)
+                {
+                    switch(i)
+                    {
+                        case 0:
+                            newFish[8] = fishAmounts[0];
+                            newFish[6] += fishAmounts[0];
+                            break;
+                        default:
+                            newFish[i-1] = fishAmounts[i];
+                            break;
+                    }
+                }
+
+                fishAmounts = newFish;
+                days--;
+            }
+
+            return fishAmounts.Sum();
+        }
+    }
+}
---
+++ Advent2021/Lanternfish/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Lanternfish;
+
+List<string> inputs = System.IO.File.ReadLines(@"c:\advent\day6\input.txt").ToList();
+
+List<int> starting = inputs.First().Split(',').Select(x => int.Parse(x)).ToList();
+
+double count = FishService.CaclulateFishV2(80, starting);
+
+Console.WriteLine(count);
+
+count = FishService.CaclulateFishV2(256, starting);
+
+Console.WriteLine(count);
